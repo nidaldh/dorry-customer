b82763a23e9a7c1c90af0bd291a7a97cddb03fe5
Nidal Dahabra <nidaldh98@gmail.com>
2024-10-22 10:13:52 +0300

    change baseUrl

--- lib/const/api_uri.dart
+++ lib/const/api_uri.dart
@@ -1,6 +1,6 @@
 class ApiUri {
-  // static const baseUrl = 'https://dorry.khidmatna.com';
-  static const baseUrl = 'http://192.168.1.12:8000';
+  static const baseUrl = 'https://dorry.khidmatna.com';
+  // static const baseUrl = 'http://192.168.1.12:8000';
   // static const baseUrl = 'http://192.168.50.206:8000';
 
   static const register = '/api/customer/register';
